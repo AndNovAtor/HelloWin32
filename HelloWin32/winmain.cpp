053ca2ff4cfa7bc81f811b61a5fae152dc04b6ff
Andrew Novikov <andnovator@gmail.com>
2014-05-03 21:33:18 +0400

    Добавлено изменение стили кисти
    *) добавил изменение стиля кисти;
    
    *) в сзвязи с предыдущим пунктом сделал переорганизацию подменю "Brush":
    внутри него добавлено вложенное меню "Color", куда перенесено изменение
    цвета, ниже - подменю "Style", куда помещены пункты выбора стиля кисти;
    
    *) подкорректирована setMenuChecks, чтобы она могла принимать на вход
    все нужные массивы - для пунктов пера, шрифта, цвета кисти и стиля
    кисти.
    
    *) добавлено использование map - своеобразного отображения (include
    <map> длбавлено, соответственно)-для пунктов подменю "кисти"; тем самым
    уменшено количество кода, т.к. пункты выбора цвета и выбора стиля по
    отдельности делают однотипные задачи;
    
    *) изменён выбо кисти в WM_PAINT - заведена функция resetBrush, которая
    "удаляет старый объект кисти" (а точнее освобождает handle) и создаёт
    новый объект; кроме того, с учётом стиля создаётся либо с помощью
    CreateSolidBrush или CreateHatchBrush;
    
    *) установка "галочек" у пунктов меню происходит в коде вместо .rc-файла
    с помощью вызовов функции setMenuChecks; также созданы константы
    дефолтных, изначальных значений цветов, стилей кисти, и т.п.  (они
    используются также иипользуются при установке "галочек")
    
    *) intColor переименована в inits (в ней происходит уже разная
    предварительная инициализация);
    
    *) изменены имена переменных и констант на более логичные (например,
    массив menuPenColor заменён на penColorMenuItems)

--- HelloWin32/winmain.cpp
+++ HelloWin32/winmain.cpp
@@ -5,6 +5,7 @@
 #include <tchar.h>
 #include "resource.h"
 #include <commdlg.h>
+#include <map>
 
 static TCHAR szWindowClass[] = _T("win32app");
 static TCHAR szTitle[] = _T("My first WinAPI App");
@@ -16,6 +17,7 @@ const int WND_HEIGHT = 400;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK dialog2Func(HWND, UINT, WPARAM, LPARAM);
 
 HWND initWindow(HINSTANCE hInstance) {
     WNDCLASS wc;
@@ -70,20 +72,29 @@ enum MyColor {
 
 const COLORREF colors[_N_COLORS] = { RGB(0, 0, 0), RGB(0, 255, 0), RGB(255, 0, 0), RGB(255,255,255) };
 HPEN pens[_N_COLORS];
-HBRUSH brushes[_N_COLORS];
+//HBRUSH brushes[_N_COLORS];
 //COLORREF fonts[_N_COLORS];
 
-int menuPenColor[_N_COLORS];
-int menuBrushColor[_N_COLORS];
-int menuFontColor[_N_COLORS];
+const int brushSolidCode = HS_DIAGCROSS + 1;
+
+const MyColor DEFAULT_PEN_COL = COL_BLACK;
+const int DEFAULT_BRUSH_COL = COL_WHITE;
+const int DEFAULT_BRUSH_STYLE = brushSolidCode;
+const MyColor DEFAULT_FONT_COL = COL_BLACK;
+
+int penColorMenuItems[_N_COLORS];
+int brushColorMenuItems[_N_COLORS];
+int brushStyleMenuItems[brushSolidCode+1];
+int fontColorMenuItems[_N_COLORS];
 
 // Current state
-MyColor currentPenColor = COL_BLACK;
-MyColor currentBrushColor = COL_WHITE;
-MyColor currentFontColor = COL_BLACK;
+unsigned int currentBrushStyle = DEFAULT_BRUSH_STYLE;
+MyColor currentPenColor = DEFAULT_PEN_COL;
+COLORREF currentBrushColor = colors[DEFAULT_BRUSH_COL];
+MyColor currentFontColor = DEFAULT_FONT_COL;
 
 COLORREF currentFontCOLORREF = RGB(0, 0, 0);
-
+HBRUSH currentBrush; //!!! !Not init! But init in _tWinMain in calling "initColors" functions
 HFONT hFontDef = NULL;
 HFONT hFontCur = NULL;
 HFONT hFontSome = CreateFont(20, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
@@ -101,30 +112,50 @@ int curXPos = 0;
 int curYPos = 0;
 RECT coordRect = { WND_WIDTH - 100, 5, WND_WIDTH, 25 };
 
-void initColors() {
+void inits() {
     for (int i = 0; i < _N_COLORS; ++i) {
         pens[i] = CreatePen(PS_SOLID, 2, colors[i]);
-        brushes[i] = CreateSolidBrush(colors[i]);
+        //brushes[i] = CreateSolidBrush(colors[i]);
         //fonts[i] = CreateFont()
     }
-    menuPenColor[COL_BLACK] = ID_PEN_BLACK;
-    menuPenColor[COL_GREEN] = ID_PEN_GREEN;
-    menuPenColor[COL_RED] = ID_PEN_RED;
-    menuPenColor[COL_WHITE] = ID_PEN_WHITE;
-    menuBrushColor[COL_BLACK] = ID_BRUSH_S_BLACK;
-    menuBrushColor[COL_GREEN] = ID_BRUSH_S_GREEN;
-    menuBrushColor[COL_RED] = ID_BRUSH_S_RED;
-    menuBrushColor[COL_WHITE] = ID_BRUSH_S_WHITE;
-    menuFontColor[COL_BLACK] = ID_FONT_BLACK;
-    menuFontColor[COL_GREEN] = ID_FONT_GREEN;
-    menuFontColor[COL_RED] = ID_FONT_RED;
-    menuFontColor[COL_WHITE] = ID_FONT_WHITE;
+    currentBrush = CreateSolidBrush(colors[COL_WHITE]);
+    
+    penColorMenuItems[COL_BLACK] = ID_PEN_BLACK;
+    penColorMenuItems[COL_GREEN] = ID_PEN_GREEN;
+    penColorMenuItems[COL_RED] = ID_PEN_RED;
+    penColorMenuItems[COL_WHITE] = ID_PEN_WHITE;
+
+    brushColorMenuItems[COL_BLACK] = ID_BRUSH_C_BLACK;
+    brushColorMenuItems[COL_GREEN] = ID_BRUSH_C_GREEN;
+    brushColorMenuItems[COL_RED] = ID_BRUSH_C_RED;
+    brushColorMenuItems[COL_WHITE] = ID_BRUSH_C_WHITE;    
+
+    fontColorMenuItems[COL_BLACK] = ID_FONT_BLACK;
+    fontColorMenuItems[COL_GREEN] = ID_FONT_GREEN;
+    fontColorMenuItems[COL_RED] = ID_FONT_RED;
+    fontColorMenuItems[COL_WHITE] = ID_FONT_WHITE;
+
+    brushStyleMenuItems[brushSolidCode] = ID_BRUSH_S_SOLID;
+    brushStyleMenuItems[HS_HORIZONTAL] = ID_BRUSH_S_HORIZONTAL;
+    brushStyleMenuItems[HS_VERTICAL] = ID_BRUSH_S_VERTICAL;
+    brushStyleMenuItems[HS_FDIAGONAL] = ID_BRUSH_S_FDIAGONAL;
+    brushStyleMenuItems[HS_BDIAGONAL] = ID_BRUSH_S_BDIAGONAL;
+    brushStyleMenuItems[HS_CROSS] = ID_BRUSH_S_CROSS;
+    brushStyleMenuItems[HS_DIAGCROSS] = ID_BRUSH_S_DIAGCROSS;
 }
 
 CHOOSECOLOR cc;                 // common dialog box structure
 const COLORREF WH = RGB(255, 255, 255);
 static COLORREF acrCustClr[16] = { WH, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH, WH }; // array of custom colors 
 //static DWORD rgbCurrent;        // initial color selection
+
+template <size_t SIZE>
+void setMenuChecks(HWND hWnd, int(&menuItemsColor)[SIZE], int currentColor) {
+    for (int i = 0; i < SIZE; ++i) {
+        UINT checked = (i == currentColor) ? MF_CHECKED : MF_UNCHECKED;
+        CheckMenuItem(GetMenu(hWnd), menuItemsColor[i], checked);
+    }
+}
 
 
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
@@ -144,6 +175,14 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
         return 1;
     }
 
+
+    inits();
+
+    setMenuChecks(hWnd, brushColorMenuItems, DEFAULT_BRUSH_COL);
+    setMenuChecks(hWnd, brushStyleMenuItems, DEFAULT_BRUSH_STYLE);
+    setMenuChecks(hWnd, penColorMenuItems, DEFAULT_PEN_COL);
+    setMenuChecks(hWnd, fontColorMenuItems, DEFAULT_FONT_COL);
+
     // Initialize CHOOSECOLOR 
     ZeroMemory(&cc, sizeof(cc));
     cc.lStructSize = sizeof(cc);
@@ -153,7 +192,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
     cc.rgbResult = RGB(255,255,255);
     cc.Flags = /*CC_FULLOPEN |*/ CC_RGBINIT;
 
-    initColors();
     // The parameters to ShowWindow explained:
     // hWnd: the value returned from CreateWindow
     // nCmdShow: the fourth parameter from WinMain
@@ -182,17 +220,27 @@ inline void redrawWindow(HWND hWnd, LPRECT rect = NULL){
     UpdateWindow(hWnd);
 }
 
-template <size_t SIZE>
-void setMenuChecks(HWND hWnd, int (&menuItemsColor)[SIZE], MyColor currentColor) {
-    for (int i = 0; i < SIZE; ++i) {
-        UINT checked = (i == currentColor) ? MF_CHECKED : MF_UNCHECKED;
-        CheckMenuItem(GetMenu(hWnd), menuItemsColor[i], checked);
-    }
-}
 
-bool chooseBrushFromArr = true;
+//bool chooseBrushFromArr = true;
 bool choosePenFromArr = true;
 bool chooseFontFromArr = true;
+
+const std::map<int, MyColor> menuId2Color = { { ID_BRUSH_C_BLACK, COL_BLACK},
+                                                { ID_BRUSH_C_GREEN, COL_GREEN},
+                                                { ID_BRUSH_C_RED, COL_RED },
+                                                { ID_BRUSH_C_WHITE, COL_WHITE}
+}; // idToColor[ID_RED] = COL_RED
+
+const std::map<int, int> menuId2Brush = { { ID_BRUSH_S_SOLID, brushSolidCode },
+                                          {ID_BRUSH_S_HORIZONTAL, HS_HORIZONTAL},
+                                          {ID_BRUSH_S_VERTICAL, HS_VERTICAL},
+                                          {ID_BRUSH_S_FDIAGONAL, HS_FDIAGONAL},
+                                          {ID_BRUSH_S_BDIAGONAL, HS_BDIAGONAL},
+                                          {ID_BRUSH_S_CROSS, HS_CROSS},
+                                          {ID_BRUSH_S_DIAGCROSS, HS_DIAGCROSS},
+}; // idToColor[ID_BRUSH_S_HORIZONTAL] = HS_HORIZONTAL
+
+void resetBrush();
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
     PAINTSTRUCT ps;
@@ -202,119 +250,166 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
 
     switch (message) {
     case WM_COMMAND:
-		wmId    = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-        switch (wmId) {
-        case ID_PEN_BLACK:
-            currentPenColor = COL_BLACK;
+        wmId = LOWORD(wParam);
+        wmEvent = HIWORD(wParam);
+        if (menuId2Color.count(wmId) > 0) {
+            MyColor tmpMyColor = menuId2Color.at(wmId);
+            currentBrushColor = colors[tmpMyColor];
+            //currentBrushColor = colors[menuId2Color[wmId]];
+            setMenuChecks(hWnd, brushColorMenuItems, tmpMyColor);
+            resetBrush();
             redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuPenColor, currentPenColor);
-            break;
-        case ID_PEN_GREEN:
-            currentPenColor = COL_GREEN;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuPenColor, currentPenColor);
-            break;
-        case ID_PEN_RED:
-            currentPenColor = COL_RED;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuPenColor, currentPenColor);
-            break;
-        case ID_PEN_WHITE:
-            currentPenColor = COL_WHITE;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuPenColor, currentPenColor);
-            break;
-        case ID_BRUSH_S_BLACK:
-            currentBrushColor = COL_BLACK;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuBrushColor, currentBrushColor);
-            break;
-        case ID_BRUSH_S_GREEN:
-            currentBrushColor = COL_GREEN;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuBrushColor, currentBrushColor);
-            break;
-        case ID_BRUSH_S_RED:
-            currentBrushColor = COL_RED;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuBrushColor, currentBrushColor);
-            break;
-        case ID_BRUSH_S_WHITE:
-            currentBrushColor = COL_WHITE;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuBrushColor, currentBrushColor);
-            break;
-        case ID_BRUSH_S_CHOOSECOLOR:
-            if (ChooseColor(&cc) == TRUE) {
-                hBrushCur = CreateSolidBrush(cc.rgbResult);
-                //rgbCurrent = cc.rgbResult;
-                chooseBrushFromArr = false;
+        } else {
+            if (menuId2Brush.count(wmId) > 0) {
+                int tmpBrushStyleCode = menuId2Brush.at(wmId);
+                currentBrushStyle = tmpBrushStyleCode;
+                setMenuChecks(hWnd, brushStyleMenuItems, tmpBrushStyleCode);
+                resetBrush();
+                redrawWindow(hWnd);
+                
+            } else {
+
+                switch (wmId) {
+                case ID_PEN_BLACK:
+                    currentPenColor = COL_BLACK;
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, penColorMenuItems, currentPenColor);
+                    break;
+                case ID_PEN_GREEN:
+                    currentPenColor = COL_GREEN;
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, penColorMenuItems, currentPenColor);
+                    break;
+                case ID_PEN_RED:
+                    currentPenColor = COL_RED;
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, penColorMenuItems, currentPenColor);
+                    break;
+                case ID_PEN_WHITE:
+                    currentPenColor = COL_WHITE;
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, penColorMenuItems, currentPenColor);
+                    break;
+                /*case ID_BRUSH_C_BLACK:
+                    currentBrushColor = colors[COL_BLACK];
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, brushColorMenuItems, COL_BLACK);
+                    break;
+                case ID_BRUSH_C_GREEN:
+                    currentBrushColor = colors[COL_GREEN];
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, brushColorMenuItems, COL_GREEN);
+                    break;
+                case ID_BRUSH_C_RED:
+                    currentBrushColor = colors[COL_RED];
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, brushColorMenuItems, COL_RED);
+                    break;
+                case ID_BRUSH_C_WHITE:
+                    currentBrushColor = colors[COL_WHITE];
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, brushColorMenuItems, COL_WHITE);
+                    break;*/
+                case ID_BRUSH_C_CHOOSECOLOR:
+                    if (ChooseColor(&cc) == TRUE) {
+                        currentBrushColor = cc.rgbResult;
+                        //rgbCurrent = cc.rgbResult;
+                        //chooseBrushFromArr = false;
+
+                    }
+                    for (int i = 0; i < sizeof(brushColorMenuItems) / sizeof(int); ++i) {
+                        CheckMenuItem(GetMenu(hWnd), brushColorMenuItems[i], MF_UNCHECKED);
+                    }
+                    resetBrush();
+                    redrawWindow(hWnd);
+                    break;
+
+                /*case ID_BRUSH_S_SOLID:
+                    currentBrushStyle = brushSolidCode;
+                    break;
+                case ID_BRUSH_S_HORIZONTAL:
+                    currentBrushStyle = HS_HORIZONTAL;
+                    break;
+                case ID_BRUSH_S_VERTICAL:
+                    currentBrushStyle = HS_VERTICAL;
+                    break;
+                case ID_BRUSH_S_FDIAGONAL:
+                    currentBrushStyle = HS_FDIAGONAL;
+                    break;
+                case ID_BRUSH_S_BDIAGONAL:
+                    currentBrushStyle = HS_BDIAGONAL;
+                    break;
+                case ID_BRUSH_S_CROSS:
+                    currentBrushStyle = HS_CROSS;
+                    break;
+                case ID_BRUSH_S_DIAGCROSS:
+                    currentBrushStyle = HS_DIAGCROSS;*/
+
+                case ID_FONT_BLACK:
+                    currentFontColor = COL_BLACK;
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, fontColorMenuItems, currentFontColor);
+                    break;
+                case ID_FONT_GREEN:
+                    currentFontColor = COL_GREEN;
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, fontColorMenuItems, currentFontColor);
+                    break;
+                case ID_FONT_RED:
+                    currentFontColor = COL_RED;
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, fontColorMenuItems, currentFontColor);
+                    break;
+                case ID_FONT_WHITE:
+                    currentFontColor = COL_WHITE;
+                    redrawWindow(hWnd);
+                    setMenuChecks(hWnd, fontColorMenuItems, currentFontColor);
+                    break;
+                case ID_FONT_CHOOSECOLOR:
+                    if (ChooseColor(&cc) == TRUE) {
+                        currentFontCOLORREF = cc.rgbResult;
+                        //rgbCurrent = cc.rgbResult;
+                        //chooseBrushFromArr = false;
+                    }
+                    for (int i = 0; i < sizeof(brushColorMenuItems) / sizeof(int); ++i) {
+                        CheckMenuItem(GetMenu(hWnd), brushColorMenuItems[i], MF_UNCHECKED);
+                    }
+                    break;
+                case ID_HELP:
+                    MessageBox(hWnd,
+                        _T("This is the short help"),
+                        _T("Help"),
+                        NULL);
+                    break;
+                case ID_HELP_DIALOG:
+                    if (!IsWindow(hDlgAbout)) {
+                        hDlgAbout = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, About);
+                        ShowWindow(hDlgAbout, SW_SHOW);
+                    }
+                    break;
+                    //case ID_DIALOG:
+                    //DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG2), hwnd, dialog2Func);
+                    //break;
+                case ID_EXIT:
+                    PostQuitMessage(0);
+                    break;
+                default:
+                    return DefWindowProc(hWnd, message, wParam, lParam);
+                }
             }
-            for (int i = 0; i < sizeof(menuBrushColor)/sizeof(int); ++i) {
-                CheckMenuItem(GetMenu(hWnd), menuBrushColor[i], MF_UNCHECKED);
-            }
-            redrawWindow(hWnd);
-            break;
-        case ID_FONT_BLACK:
-            currentFontColor = COL_BLACK;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuFontColor, currentFontColor);
-            break;
-        case ID_FONT_GREEN:
-            currentFontColor = COL_GREEN;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuFontColor, currentFontColor);
-            break;
-        case ID_FONT_RED:
-            currentFontColor = COL_RED;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuFontColor, currentFontColor);
-            break;
-        case ID_FONT_WHITE:
-            currentFontColor = COL_WHITE;
-            redrawWindow(hWnd);
-            setMenuChecks(hWnd, menuFontColor, currentFontColor);
-            break;
-        case ID_FONT_CHOOSECOLOR:
-            if (ChooseColor(&cc) == TRUE) {
-                currentFontCOLORREF = cc.rgbResult;
-                //rgbCurrent = cc.rgbResult;
-                chooseBrushFromArr = false;
-            }
-            for (int i = 0; i < sizeof(menuBrushColor) / sizeof(int); ++i) {
-                CheckMenuItem(GetMenu(hWnd), menuBrushColor[i], MF_UNCHECKED);
-            }
-            break;
-        case ID_HELP:
-            MessageBox(hWnd,
-                _T("This is the short help"),
-                _T("Help"),
-                NULL);
-            break;
-        case ID_HELP_DIALOG:
-            if (!IsWindow(hDlgAbout)) {
-                hDlgAbout = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, About);
-                ShowWindow(hDlgAbout, SW_SHOW);
-            }
-            break;
-        case ID_EXIT:
-            PostQuitMessage(0);
-            break;
-		default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
-		}
+        }
     case WM_PAINT: {
 
         hdc = BeginPaint(hWnd, &ps);
         
         SelectObject(hdc, pens[currentPenColor]);
-        if (chooseBrushFromArr) {
+        /*if (currentBrushStyle < brushSolidCode) {
             SelectObject(hdc, brushes[currentBrushColor]);
         } else {
             SelectObject(hdc, hBrushCur);
             chooseBrushFromArr = true;
-        }
+        }*/
+        SelectObject(hdc, currentBrush);
         //SelectObject(hdc, brushes[currentBrushColor]);
         //SelectObject(hdc, fonts[currentFontColor]);
         //SetTextColor(hdc, fonts[currentColor]);
@@ -356,7 +451,34 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
     return 0;
 }
 
+void resetBrush() {
+    DeleteObject(currentBrush);
+    if (currentBrushStyle < brushSolidCode) {
+        currentBrush = CreateHatchBrush(currentBrushStyle, currentBrushColor);
+    } else {
+        currentBrush = CreateSolidBrush(currentBrushColor);
+    }
+}
+
+
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message) {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK dialog2Func(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
     switch (message) {
